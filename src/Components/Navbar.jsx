6a34790f4ba018dec514f613b985071999dac6d7
Mason Fancher <mason.fancher.jp@outlook.com>
2026-03-12 22:58:17 +0900

    add global card search, modal preview, and mobile responsive navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,22 @@
 import { useNavigate } from "react-router-dom"
 import vaultIcon from "../assets/vault.png"
-import {useState, useEffect} from "react"
+import {useState, useEffect, useContext} from "react"
+import { CardContext } from "../CardContext"
+
+
+
+
 export default function Navbar() {
 
+
+const {collection, addCard} = useContext(CardContext)
 const Navigate = useNavigate()
 const [input, setInput] = useState("")
 const [results, setResults] = useState([])
+const [selectedCard, setSelectedCard] = useState(null)
+
+
+
   useEffect(() => {
 
  async function SearchBar(){
@@ -15,8 +26,6 @@ const [results, setResults] = useState([])
  }
 
  SearchBar()
- 
-
   }, [input])
 
 
@@ -30,19 +39,84 @@ const [results, setResults] = useState([])
         TCG Vault
       </h1>
 
-      <div className="nav-search">
+
+    <div className="nav-search">
         <input
           value={input}
           onChange={((e) => setInput(e.target.value))}
+          onBlur={() => setResults([])}
           type="text"
           placeholder="Search Pokémon cards..."
         />
+
+      {results.length > 0 && (
+        <div className="drop-down-results">
+        {results.map((result, index) => {
+        return (
+            <div
+                onMouseDown={() => setSelectedCard(result)}
+                key={index}
+                 >
+                {result.name}
+            </div>
+                 )
+                 })}
+        </div>
+        )}
+
+
+     </div>
+
+
+    <div className="nav-actions">
+        <button  
+            onClick={() => Navigate('/collection')}
+            className="nav-btn signup"> View Collection</button>
+        </div>
+
+    {selectedCard !== null &&(
+    <div className="modal-overlay">
+        
+          
+      
+
+      <div className="modal-card">
+          <img src={`${selectedCard.image}/high.webp`} alt={selectedCard.name} />
+          <h4>{selectedCard.name}</h4>
+           <p>#{selectedCard.localId}</p>
+           <button
+               className={collection.some(c => c.id === selectedCard.id) ? "add-card-btn added" : "add-card-btn"}
+               onClick={() => addCard(selectedCard)}
+              >
+        {collection.some(c => c.id === selectedCard.id) ? "✓" : "+"}
+          </button>
+          <button
+        className="modal-close-btn"
+        onClick={() => setSelectedCard(null)}>Close
+          </button>
       </div>
 
-      <div className="nav-actions">
-            <button className="nav-btn login">Log In</button>
-        <button className="nav-btn signup">Sign Up</button>
-      </div>
+     
+
+
+
+    </div>
+    )}
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
     </section>
   )
 }
